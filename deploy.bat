aff5c4ad02e8a512d838e5cd78caa7d1849395bf
kmkarakaya <kmkarakaya@gmail.com>
2024-08-03 18:40:42 +0300

    deploy

--- deploy.bat
+++ deploy.bat
@@ -4,16 +4,17 @@ setlocal
 :: Run pkg and check for errors
 echo "Running pkg..."
 ::pkg .
+echo "pkg command executed with errorlevel %errorlevel%"
 if %errorlevel% neq 0 (
     echo "Error: pkg command failed."
     exit /b %errorlevel%
 )
-echo "pkg command completed successfully."
-pause
+::echo "pkg command completed successfully."
 
 :: Add changes to git and check for errors
 echo "Running git add..."
 git add .
+echo "git add executed with errorlevel %errorlevel%"
 if %errorlevel% neq 0 (
     echo "Error: git add command failed."
     exit /b %errorlevel%
@@ -23,6 +24,7 @@ echo "git add completed successfully."
 :: Commit changes to git and check for errors
 echo "Running git commit..."
 git commit -m "deploy"
+echo "git commit executed with errorlevel %errorlevel%"
 if %errorlevel% neq 0 (
     echo "Error: git commit command failed."
     exit /b %errorlevel%
@@ -32,6 +34,7 @@ echo "git commit completed successfully."
 :: Push changes to the remote repository and check for errors
 echo "Running git push..."
 git push origin main
+echo "git push executed with errorlevel %errorlevel%"
 if %errorlevel% neq 0 (
     echo "Error: git push command failed."
     exit /b %errorlevel%
@@ -40,3 +43,4 @@ echo "git push completed successfully."
 
 echo "Deployment completed successfully."
 endlocal
+pause
